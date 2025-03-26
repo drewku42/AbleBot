6d70ff4f71d7ff35435d093546edd3f3f88d45be
Drew Meyer <drewmeyer28@gmail.com>
2025-03-26 14:31:14 -0500

    Removed unneeded files, focusing on getting running ChatBot in 1 module

--- utils/retrieval.py
+++
@@ -1,9 +0,0 @@
-# init.py
-# This script is used to instantiate vector database.
-from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings.openai import OpenAIEmbeddings
-
-def load_retriever(query):
-    new_db = FAISS.load_local("faiss_index", embeddings=OpenAIEmbeddings())
-    docs = new_db.similarity_search(query)
-    return docs
